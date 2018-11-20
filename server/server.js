51e6da5bdf69fee50b6f34c16f988e000475a5d4
code <chetan.dmc@gmail.com>
2018-11-21 00:00:35 +0530

    Server setup

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+const port = process.env.PORT || 3002;
+app.listen(port, () => {
+    console.log(`Server Running at ${port}`)
+})
